9dbcc146a46e667ce34764f87b1fa557080ae636
unknown <aindriu80@gmail.com>
2020-04-03 14:50:20 +0100

    Fetching content from JSON API

--- src/App.js
+++ src/App.js
@@ -1,37 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import './App.css';
 
-
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
 
     this.state = {
-      monsters: [
-        {name: 'Frankenstein',
-        id:'asc1'},
-        {name: 'Dracula',
-      id:'asc2'},
-        {name: 'Zombie',
-      id:'asc3'}
-      ]
+      monsters: [],
     };
-  }  
+  }
 
+  componentDidMount() {
+    fetch('http://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
 
-    
-
-render() {
-  return (
-    
-    <header className="App-header">
-       
-    {
-      this.state.monsters.map(monster =><h1 key={monster.id}>{monster.name}</h1>)
-    }
-    </header>
-    
-  )};
-};
+  render() {
+    return (
+      <header className="App-header">
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </header>
+    );
+  }
+}
 
 export default App;
