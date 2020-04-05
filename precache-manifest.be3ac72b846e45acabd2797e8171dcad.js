2192a80ccc52412cd15d8f6a50c9acd605d3e87a
unknown <aindriu80@gmail.com>
2020-04-05 22:01:01 +0100

    Updates

---
+++ precache-manifest.be3ac72b846e45acabd2797e8171dcad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a5f9ac466f89354ea9a62a93a1f0041",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f49cdc1ecc0e6033c44d",
+    "url": "/monsters-rolodex/static/css/main.31be72eb.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f49cdc1ecc0e6033c44d",
+    "url": "/monsters-rolodex/static/js/main.c0452b85.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
